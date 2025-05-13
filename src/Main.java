f29a0a7cfab65dcc1156064ff29cc533d7bdf23f
brian070069 <kkipngeno409@gmail.com>
2025-05-13 15:07:49 +0300

    refined commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Printer printer = new Printer(50,true);
-        System.out.println("initial page count="+printer.getPagesPrinted());
+        Printer printer = new Printer(50,false);
+        System.out.println("Initial page count ="+printer.getPagesPrinted());
 
-        int pagesPrinted=printer.printPages(5);
-        System.out.printf("Current job pages = %d ,printer total :%d %n",pagesPrinted,printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current job pages : %d , Printer total : %d%n",
+                pagesPrinted, printer.getPagesPrinted());
 
-        pagesPrinted=printer.printPages(10);
-        System.out.printf("Current job pages = %d ,printer total :%d %n",pagesPrinted,printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(10);
+        System.out.printf("Current job pages : %d , Printer total : %d%n",
+                pagesPrinted, printer.getPagesPrinted());
 
     }
 }
